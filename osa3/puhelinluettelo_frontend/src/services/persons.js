71050aafcfa31d44e15e9e984efe2003f7647d5f
Esa Taberman <138495098+e-taberman@users.noreply.github.com>
2025-05-19 20:55:02 +0300

    updated puhelinluettelo

--- osa3/puhelinluettelo_frontend/src/services/persons.js
+++ osa3/puhelinluettelo_frontend/src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const serverUrl = "http://localhost:3001/api/persons/"
+// const serverUrl = "http://localhost:3001/api/persons/"
+const serverUrl = "/api/persons/"
 
 const getAll = () => {
     const request = axios.get(serverUrl)
--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -4,8 +4,8 @@ var morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
-
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token("body", (request) => {
     return JSON.stringify(request.body)
